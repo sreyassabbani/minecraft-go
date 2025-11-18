6ae23d884db026b8c67b0c95336991a1d8b3ce2c
Sreyas Sabbani <sreyassabbani@gmail.com>
2025-11-18 08:24:19 -0500

    minor feat(algebra): matrix, etc

--- host/main.cpp
+++ host/main.cpp
@@ -1,20 +1,23 @@
 #include "../utils/algebra.h"
 #include "../utils/general.h"
 #include <iostream>
+#include <array>
 
 using algebra::Vector;
+using std::array;
 
 int main() {
     std::cout << "Host harness for utils" << std::endl;
 
-    Vector<int> a({ 1, 2, 3 });
-    Vector<int> b({ -2, 4, 0 });
+    Vector<int, 3> a = array<int, 3>{ 1, 2, 3 };
+    Vector<int, 3> b = array<int, 3>{ -2, 4, 0 };
+    
     auto c = a + b;
 
     println("Size check: ", c.size());
     println("Element check: ", c[0], ", ", c[1], ", ", c[2]);
 
-    Vector<int> moved = std::move(c);
+    Vector<int, 3> moved = std::move(c);
     println("Move check, size after move: ", moved.size());
 
     return 0;
--- utils/algebra.h
+++ utils/algebra.h
@@ -3,37 +3,39 @@
 
 #include <cassert>
 #include <utility>
-#include <vector>
-using namespace std;
+#include <array>
+
+using std::array;
 
 namespace algebra {
 
-template <typename T> class Vector {
-    vector<T> data;
+template <typename T, size_t LENGTH> class Vector {
+    array<T, LENGTH> data;
 
 public:
     // not marked `explicit` (`implicit` should have been an opt-in feature)
-    Vector(vector<T>& v) : data(v) {}
+    // Vector(array<T, LENGTH> v) : data(v) {}
 
     // copy constructor (takes a ref to an object of the same class as its
     // argument)
-    Vector(const vector<T>& v) : data(v) {}
-    Vector(const Vector<T>& other) : data(other.data) {}
+    Vector(const array<T, LENGTH>& v) : data(v) {}
+    // Vector(const Vector<T, LENGTH>& other) : data(other.data) {}
 
     // move constructor (takes a ref to an existing object, an rvalue ref, from
     // whom ownership of resources is transferred). after the function call,
     // `other.data` will reach a valid but unspecified state; that state is
     // typically empty, which is correctly handled by `std::move`
-    Vector(Vector<T>&& other) noexcept : data(std::move(other.data)) {}
+    Vector(Vector<T, LENGTH>&& other) noexcept : data(std::move(other.data)) {}
 
-    size_t size() const { return data.size(); }
+    // `const` and/or `noexcept` and/or `constexpr` and/or `static`?? also aren't some of these inferred by the compiler
+    static constexpr size_t size() const { return LENGTH; }
 
     const T& operator[](size_t idx) const {
         assert(idx < data.size());
         return data[idx];
     }
 
-    T& operator[](size_t idx) {
+    T& operator[](size_t idx) const {
         assert(idx < data.size());
         return data[idx];
     }
@@ -56,6 +58,19 @@ public:
             res.data[i] += other.data[i];
         }
         return res;
+    }
+};
+
+template <typename T, size_t ROWS, size_t COLS>
+class Matrix {
+    array<T, ROWS * COLS> data;
+public:
+    size_t rows() {
+        return ROWS;
+    }
+
+    size_t cols() {
+        return COLS;
     }
 };
 
