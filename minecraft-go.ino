a4b7dc6e91aff0d0bd5bf31c7ec8b14188233f76
Sreyas Sabbani <sreyassabbani@gmail.com>
2025-11-17 19:24:51 -0500

    chore: reconfigure C/C++ intellisense and include `Arduino.h` which is detected in `includePath`

--- minecraft-go.ino
+++ minecraft-go.ino
@@ -1,18 +1,18 @@
+#include <Arduino.h>
 #include <utils/general.h>
-#include <cstdint>
 
 const int L = 10;
 bool blocks[L * L * L];
 
 // 480x320
-uint8_t map[480 * 320];
+int map[480 * 320];
 
 // (i, j, k)
-projected = (i, j, k) - n.scale((camera_center - (i, j, k)).dot(n) / n.magsq());
+// projected = (i, j, k) - n.scale((camera_center - (i, j, k)).dot(n) / n.magsq());
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
